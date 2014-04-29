93bd36ec45fac2d5d819e8f2c37e2c7faf1a0ce3
andyrizzuto <andyrizzuto@gmail.com>
2014-04-29 21:31:42 +0000

    first commit

---
+++ client/helpers/errors.js
@@ -0,0 +1,9 @@
+// Local (client-only) collection
+Errors = new Meteor.Collection(null);
+
+// Write messages to local miniMongo using this syntax in any .js
+// throwError = function(message) {
+//  Errors.insert({message: message})
+// }
+//
+
